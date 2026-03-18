8cd98f3fc5b9ea257ac6205a1cdbe80cbcee5a7d
Shahadat429 <shahadathossan429@gmail.com>
2026-03-18 10:09:47 +0600

    update

--- server/server.js
+++ server/server.js
@@ -24,7 +24,8 @@ await connectCloudinary();
 
 //allowed multiple origins
 const allowedOrigins = [
-  'http://localhost:5173'
+  'http://localhost:5173',
+  'https://grocery-e-commerce-website.vercel.app'
 ];
 
 app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhook);
